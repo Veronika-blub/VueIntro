f7ce6da197cc9225d5b81f4b829887f2955b3301
Veronika-blub <pu74vys@eduvaud.ch>
2025-11-04 15:45:59 +0100

    L3-start

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
-// const product = 'Socks'
-
-
 const app = Vue.createApp({
     data() {
-        return{
-            product: 'Bottes'
+        return {
+            product: 'Socks'
         }
     }
 })
