dbf224f17726e44863f4a622a1dcec4c1764a304
Veronika-blub <pu74vys@eduvaud.ch>
2025-11-04 15:53:53 +0100

    2.7 L3

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            description: 'Ajouter une description à l\'objet de données.'
         }
     }
 })
