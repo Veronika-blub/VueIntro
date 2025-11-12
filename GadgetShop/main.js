5769a478a05d500e9e52eb0329ca0f70772762e9
Veronika-blub <pu74vys@eduvaud.ch>
2025-11-12 09:30:46 +0100

    GadgetShop step4

--- GadgetShop/main.js
+++ GadgetShop/main.js
@@ -4,10 +4,15 @@ const app = Vue.createApp({
             details: "Découvrez les derniers gadgets électroniques de haute technologie!",
             //["Free Shipping", "30-day Return", "1 Year Warranty"],
             title: "Vue Gadget Shop",
-            name: 'Smartphone XZ',
-            priceInit: 799,
-            image: "./assets/phone.jpg",
-            inStock: true
+            // name: 'Smartphone XZ',
+            // priceInit: 799,
+            // image: "./assets/phone.jpg",
+            // inStock: true,
+            gadgets: [
+                { id: 1, name: "Smartphone XZ", price: 799, image: "./assets/phone.jpg", inStock: true },
+                { id: 2, name: "Laptop Mat' shatal", price: 999, image: "./assets/laptop.jpg", inStock: false },
+                { id: 3, name: "Earbuds Proty abortov", price: 199, image: "./assets/earbuds.jpg", inStock: true }
+            ]
         }
     },
     methods: {
@@ -16,6 +21,9 @@ const app = Vue.createApp({
     computed:{
         price() {
             return this.priceInit + ' €'
+        },
+        stock() {
+            return this.inStock ? '✅ En stock' : '❌ En rupture de stock'
         }
     }
 })
